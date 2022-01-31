fe81b768940939b2d8d98dbee799ca994d461529
MM-KillyRex <85969013+MM-KillyRex@users.noreply.github.com>
2022-01-31 13:17:53 -0600

    Formato terminado Excel

--- src/services/query.service.js
+++ src/services/query.service.js
@@ -61,7 +61,7 @@ const getStatusKof = async (sIds, dtFrom, dtTo) => {
      "iConsumoAcumuladoFinal",
      "dRendEcmMin",
      "dRendEcmProm",
-     "dRendEcmMax",
+     "dRendEcmMax", // 38
      "dRendEcmTotal",
      "iPanicoEventos",
      "iPanicoMaximaG", 
@@ -136,23 +136,36 @@ const getStatusKof = async (sIds, dtFrom, dtTo) => {
         let LitrosConsumidos = datos[2];
         let MaximaTempDuracion = datos[5];
         let MinimaTempDuracion = datos[6];
+        let tempProm = datos[7];
         let RendCalcMin = datos[8];
         let RendCalcTotal = datos[9];
         let RendCalcMax = datos[10];
+        let TotCodigosFalla = datos[14];
         let RpmDurSobreRevolucion = datos[19];
         let RpmMaxRegistradas = datos[20];
+        let iRalentiTiempo = datos[21];
         let iRalentiSumTiempo = datos[22];
         let iRalentiSumTotal = datos[23];
         let iRalentiMaximoTiempo = datos[24];
-        let iRalentivsTiempoOperacion = [25];
+        let iRalentivsTiempoOperacion = datos[25];
         let iAceleracionDuracion = datos[28];
         let iVelocidadPromedio = datos[29];
+        let iVelocidadMaxima = datos[30];
         let dCo2Generadokg = datos[31];
         let Co2GeneradoKgKm = datos[32];
         let KmFinal = datos[33];
+        let iConsumoAcumuladoInicial = datos[34];
+        let iConsumoAcumuladoFinal = datos[35];
+        let dRendEcmMin = datos[36];
+        let dRendEcmProm = datos[37];
+        let dRendEcmMax = datos[38];
+        let dRendEcmTotal = datos[39];
+        let iPanicoEventos = datos[40];
         let iPanicoMaximaG = datos[41];
+        let iGiroBruscoEventos = datos[42];
         let iGiroBruscoMaxiGIzq = datos[43];
         let iGiroBruscoMaxiGDer = datos[44];
+        let iAceleracionEventos = datos[45];
         let iAceleracionMaximaG = datos[46];
         let dExcepcionRalentiUODuracion = datos[47];
         let dCalificacion = datos[48];
@@ -164,6 +177,8 @@ const getStatusKof = async (sIds, dtFrom, dtTo) => {
         
         // Variables Auxiliares
         let num;
+        let valiRalentivsTiempoOperacion;
+        let valTempProm;
         let valueProm;
         let valCalcMin;
         let valEvRev;
@@ -178,11 +193,6 @@ const getStatusKof = async (sIds, dtFrom, dtTo) => {
         let valCalificacion;
         let stringVal;
 
-        //Pendiente // Máxima duración de evento [hh:mm:ss] [15]
-        //datos = [datos.slice(0,5), i, ...datos.slice(5)]; 
-
-
-        //const datos2 = datos.slice(5); Obtener el arreglo de 5 elems
 
 
         let distanceTrips = KmRecorridos;
@@ -216,127 +226,134 @@ const getStatusKof = async (sIds, dtFrom, dtTo) => {
         distanceTrips = LitrosConsumidos;
         Math.round(num8 / distanceTrips, 2) // ?
 
-        // Operación debajo de temperatura mínima recomendada Celda 17
+        // Máxima duración de evento Celda 16
         num = validar(MaximaTempDuracion)
-   
-        // Insercion valor
-        insert(datos, 5, num); // ?
+        const promTemp0 = Duration.fromMillis(valueProm).toFormat('hh:mm:ss.SSSS').toString();
+        console.log("MaxDurEvent: "+ promTemp0)
+        insert(datos, 5, promTemp0);
+
+        // Operación debajo de temperatura mínima recomendada Celda 17
+        valCalcMin = validar(MinimaTempDuracion);
+        const rendCal = Duration.fromMillis(valCalcMin).toFormat('hh:mm:ss.SSSS').toString();
+        insert(datos, 6, rendCal);
+        console.log("Operacion por de Temp: "+rendCal);
         
         // Insersion valor Máxima duración de evento debajo de temperatura mínima de operación Celda 18
         valueProm = validar(TiempoTemperatura); 
-        // Insercion valor 
-        const promTemp = Duration.fromMillis(valueProm).toFormat('hh:mm:ss').toString();
-        insert(datos, 7, promTemp); // ?
-    
-        //insert(datos, 7, promTemp);
+        const promTemp = Duration.fromMillis(valueProm).toFormat('hh:mm:ss.SSSS').toString();
+        insert(datos, 7, promTemp);
+        console.log("Max dur event: "+promTemp);
         
-        // Temperatura promedio de operación Celda 19
-        ////// distanceTrips = MinimaTempDuracion;
-        valCalcMin = validar(MinimaTempDuracion);
-        const rendCal = Duration.fromMillis(valCalcMin).toFormat('hh:mm:ss');
-        insert(datos, 6, rendCal);
-        console.log(rendCal)
+        //Celda 19 Temp prom 
+        valTempProm = validar(tempProm);
+        insert (datos, 8, valTempProm); 
 
         // dRendCalcMin Celda 20
         distanceTrips = RendCalcMin;
         const round = Math.round(distanceTrips, 2);
-        datos[8] = round;
+        datos[9] = round;
 
         // dRendCalcTotal Celda 21
-        distanceTrips = RendCalcTotal;
-        const round2 = Math.round(distanceTrips, 2);
-        datos[9] = round2;
+        RendCalcTotal = 0;
+        const round2 = Math.round(RendCalcTotal, 2);
+        datos[10] = round2;
 
         // dRendCalcTotal Celda 22
         distanceTrips = RendCalcMax;
         const round3 = Math.round(distanceTrips, 2);
-        datos[10] = round3;
+        datos[11] = round3;
+        
+        //Total de códigos de falla activos Celda 26
+        const valTotalCod = validar(TotCodigosFalla);
+        insert(datos, 15, valTotalCod);
+        console.log("Total Codigos de falla Activos" + valTotalCod);
 
-        // Duración total de los eventos de sobre revolución 31
+        // Duración total de los eventos de sobre revolución  Celda 31
         valEvRev = validar(RpmDurSobreRevolucion);
-        /* 
-        if(distanceTrips !== null || distanceTrips !== undefined) {
-            distanceTrips = RpmDurSobreRevolucion;
-            valEvRev = distanceTrips;
-        } else {
-            valEvRev = 0;
-        }
-        */
-        //insersion
         const eventosRev = Duration.fromMillis(valEvRev).toFormat('hh:mm:ss').toString();
-        insert(datos, 19, eventosRev);
-        // datos[19] = eventosRev
+        insert(datos, 20, eventosRev);
 
-        // Máximas RPM´s registradas
+        // Máximas RPM´s registradas Celda 32
         valMAXRPM = validar(RpmMaxRegistradas)
-        insert(datos, 20, valMAXRPM);
-        //datos[20]  = distanceTrips;
+        insert(datos, 21, valMAXRPM);
 
-        // Tiempo en ralentí (excedentes a 5min/evento) // Celda 34
+        // Cantidad de eventos Ralentí >5 min Celda 33
+        const valRalentiTemp =validar(iRalentiTiempo);
+        insert(datos, 22, valRalentiTemp);
+        console.log(valRalentiTemp);
+
+        // Tiempo en ralentí (excedentes a 5min/evento)  Celda 34
         valRalentiSum = validar(iRalentiSumTiempo)
-        /* 
-        if(distanceTrips !== null || distanceTrips !== undefined) {
-            distanceTrips = iRalentiSumTiempo;
-            valRalentiSum = distanceTrips;
-        } else {
-            valRalentiSum = 0;
-        }
-        */
         const realentiSumT = Duration.fromMillis(valRalentiSum).toFormat('hh:mm:ss').toString();
-        insert(datos, 22, realentiSumT);
-        // datos[22] = realentiSumT
+        insert(datos, 23, realentiSumT);
+
 
         // Tiempo total de ralentí  // Celda 35
         valRalentiSumTotal = validar(iRalentiSumTotal)
         const realentiSumTot = Duration.fromMillis(valRalentiSumTotal).toFormat('hh:mm:ss').toString();
-        insert(datos, 23, realentiSumTot);
-        // datos[23] = realentiSumTot
+        insert(datos, 24, realentiSumTot);
+    
 
         // Tiempo Ralenti UO --> Celda 36
         distanceTrips = iRalentiMaximoTiempo;
         valRalentiMaxT = validar(iRalentiMaximoTiempo)
-        const ralentiMaxTime = Duration.fromMillis(valRalentiMaxT).toFormat('hh:mm:ss').toString();
-        insert(datos, 24, ralentiMaxTime);
-        // datos[24] = ralentiMaxTime
-        // Tiempo Ralenti Fuera UO  --> Celda 37
-         const dExcepcionRal = Duration.fromMillis((dExcepcionRalentiUODuracion !== null || dExcepcionRalentiUODuracion !== undefined ? dExcepcionRalentiUODuracion : 0)).toFormat('hh:mm:ss.SSS');
-         insert(datos, 25, dExcepcionRal);
-         // Máximo tiempo en ralentí (1 solo evento) 38
+       
+        //const ralentiMaxTime = Duration.fromMillis(valRalentiMaxT).toFormat('hh:mm:ss').toString();
+        insert(datos, 25, realentiSumT);
+
+        // Duración total de los eventos de sobre revolución  Celda 31
+        valEvRev = validar(RpmDurSobreRevolucion);
+        const eventosRev2 = Duration.fromMillis(valEvRev).toFormat('hh:mm:ss').toString();
+        insert(datos, 26, eventosRev2);
+
+        // Tiempo Ralenti Fuera UO  --> Celda 38
+         const dExcepcionRal = Duration.fromMillis((dExcepcionRalentiUODuracion !== null || dExcepcionRalentiUODuracion !== undefined ? dExcepcionRalentiUODuracion : 0)).toFormat('hh:mm:ss');
+         insert(datos, 27, dExcepcionRal);
+    
          // Tiempo en ralenti vs Tiempo de Operación  --> Celda 39
-        distanceTrips = iRalentivsTiempoOperacion;
-        valRalentivsTiempoOp = validar(iRalentivsTiempoOperacion);
-        const realentiVsTiempo = Math.round(valRalentivsTiempoOp, 2);
-        insert(datos, 26, realentiVsTiempo);
-         // datos[26] = realentiVsTiempo
+        valiRalentivsTiempoOperacion = validar(iRalentivsTiempoOperacion);
+        //const realentiVsTiempo = Math.round(valiRalentivsTiempoOperacion, 2);
+        insert(datos, 28, valiRalentivsTiempoOperacion);
+        console.log("ralenti vs Tiempo de Operación" + valiRalentivsTiempoOperacion)
         
-         // Tiempo de Manejo --> 40
+        
+         // Tiempo de Manejo --> Celda 40
          const obj = validar(TiempoManejo);
          const TimeDriving = Duration.fromMillis(obj).toFormat('hh:mm:ss').toString();
-         insert(datos, 27, TimeDriving);
-         // datos[27] = TimeDriving
+         insert(datos, 29, TimeDriving);
+        
+         // Tiempo de Operación -- Celda 41
+         const valorSuma = iRalentiSumTotal + TiempoManejo;
+         const top = Duration.fromMillis(valorSuma).toFormat('hh:mm:ss');
+         insert(datos, 30, top);
+        
+         /*
+         
+         const tiempoOpMilis = tiempoOperacion * 1000 
+         console.log("tiempoOpMilis dd" + tiempoOpMilis )
+          = Duration.fromMillis(tiempoOpMilis).toFormat('hh:mm:ss').toString();
+         insert(datos, 30,TiempoMili);
+        */
+         
 
-         const  tiempoOp =  validar(HorasMotor);
-         const TiempoMili = Duration.fromMillis(tiempoOp).toFormat('hh:mm:ss').toString();
-         insert(datos, 28,TiempoMili);
-
-         // datos[29]
-         // datos[30]
+         // datos[31]
+         // datos[31]
 
          valAcelera = validar(iAceleracionDuracion);
-         const aceleracionDur = moment(valAcelera).milliseconds
-         insert(datos, 31, aceleracionDur);
+         const aceleracionDur = Duration.fromMillis(valAcelera).toFormat('hh:mm:ss').toString();
+         insert(datos, 33, aceleracionDur);
 
-         insert(datos, 32, iVelocidadPromedio);
-         // datos[32] = distanceTrips
-         // 33 velocidad max
+         insert(datos, 34, iVelocidadPromedio);
+         insert(datos, 35, iVelocidadMaxima);
+
     
          valCo2Generadokg = validar(dCo2Generadokg);
          const co2Gen = Math.round(valCo2Generadokg, 2);
-         insert(datos, 33, co2Gen);
+         insert(datos, 36, co2Gen);
 
          valCo2Generadokm = validar(Co2GeneradoKgKm);
          const co2GenKm = Math.round(valCo2Generadokm, 2);
-         insert(datos, 34, co2GenKm);
+         insert(datos, 37, co2GenKm);
 
          distanceTrips = Co2GeneradoKgKm;
 
@@ -345,43 +362,68 @@ const getStatusKof = async (sIds, dtFrom, dtTo) => {
                  valCalificacion = dCalificacion;
                  stringVal = valCalificacion + "";
              } else if(bValida) {
-                valCalificacion = stringVal ;
+                valCalificacion = stringVal;
                 stringVal = valCalificacion + "";
              } else{
                  stringVal = "NA";
              }
              
+            } else {
+                stringVal = "82.00";
             }
 
-        insert(datos, 35, stringVal);
-             ///36 - 43
+        insert(datos, 38, stringVal);
+        insert(datos, 39, KmFinal);
+        insert(datos, 40, iConsumoAcumuladoInicial);
+        insert(datos, 41, iConsumoAcumuladoFinal);
 
+        //Rendimiento ECM mínimo [km/L]
+        insert(datos, 42, dRendEcmMin);
+        //Rendimiento ECM promedio
+        insert(datos, 43, dRendEcmProm);
+        //Rendimiento ECM máximo
+        insert(datos, 44, dRendEcmMax);
+        //Rendimiento ECM total
+        insert(datos, 45, dRendEcmTotal);
+
+        let valPanicEvents = validar(iPanicoEventos);
+        insert(datos, 46, valPanicEvents);
+        
         let valPanicoMax = validar(iPanicoMaximaG);
-        const panicoMaxG = Math.round(valPanicoMax,2);
-        insert(datos, 44, panicoMaxG); 
-        // 45 - giro brusco
+        //const panicoMaxG = Math.round(valPanicoMax,2);
+        insert(datos, 47, valPanicoMax); 
+
+        let valEventosGiroBrusco = validar(iGiroBruscoEventos);
+        insert(datos, 48, valEventosGiroBrusco);
 
         let valGiroBruscoMaxiGIzq = validar(iGiroBruscoMaxiGIzq);
-        const giroBruscoIzq = Math.round(valGiroBruscoMaxiGIzq,2);
-        insert(datos, 46, giroBruscoIzq);
+        //const giroBruscoIzq = Math.round(valGiroBruscoMaxiGIzq,2);
+        insert(datos, 49, valGiroBruscoMaxiGIzq);
     
         let valGiroBruscoMaxiGDer = validar(iGiroBruscoMaxiGDer);
-        const giroBrusco = Math.round(valGiroBruscoMaxiGDer,2);
-        insert(datos, 47, giroBrusco);
+        //const giroBrusco = Math.round(valGiroBruscoMaxiGDer,2);
+        insert(datos, 50, valGiroBruscoMaxiGDer);
         
-        // 48 iAceleracionEvento
+        let valAceleracionEvents = validar(iAceleracionEventos);
+        insert(datos, 51, valAceleracionEvents);
              
         let valAceleracionMaximaG = validar(iAceleracionMaximaG);
-        const aceleracionMax = Math.round(valAceleracionMaximaG,2);
-        insert(datos, 49, aceleracionMax);
+        //const aceleracionMax = Math.round(valAceleracionMaximaG,2);
+        insert(datos, 52, valAceleracionMaximaG);
         
         let datosXD = 0;
         let comunicando = 1;
-        insert(datos, 50, datosXD);
+        insert(datos, 53, datosXD);
         let fechaNew = Duration.fromMillis(datosXD).toFormat('hh:mm:ss').toString();
-        insert(datos, 51, fechaNew);
-        insert(datos, 52, 1);     
+        insert(datos, 54, fechaNew);
+        insert(datos, 55, comunicando);   
+        
+       datos[56] = " ";
+       datos[57] = " ";
 
+        
+
+        console.log(datos);
         return ([economico, tipo, marca, modelo, year, noSerie, emplazamiento,date, week, can,  ...datos]) // spread 
     });
 
@@ -389,7 +431,8 @@ const getStatusKof = async (sIds, dtFrom, dtTo) => {
 
 };
 
-const insert = (array, i, item) => [...array.slice(0, i), item, ...array.slice(i)];
+const insert = (array, i, item) =>  array.splice(i, 1, item);
+
 
 const fillChildren = (group, groupsDict, levels = 2) => {
     if(levels > 1) {
@@ -416,17 +459,6 @@ const generateXLSX = async (Ids, dtTo, dtFrom) => {
     return wb.outputAsync();
 }
 
-/*
-const generateXLSX = () => {
-    xlsx.fromBlankAsync('docs/template.xlsx')
-    .then(workbook => {
-        const report = getStatusKof();
-        const value = workbook.sheet("Sheet1").cell("A1").value(report);
-
-        console.log(value);
-    })
-}
-*/
 const validar = valor => valor || 0;
 
 module.exports = {
